8f228f8ef8b87a7142b487e95e3f1fa66a2486a8
ShrutikaSingh <shrutika051220@gmail.com>
2024-08-08 23:20:21 -0700

    complete map with kafka

---
+++ static/leaf.js
@@ -0,0 +1,43 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11', // Updated to match the new Mapbox style ID format
+  accessToken: MAP_ACCESS_TOKEN // ENTER YOUR ACCESS TOKEN HERE
+}).addTo(mymap);
+
+mapMarkers1 = [];
+mapMarkers2 = [];
+mapMarkers3 = [];
+//https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?MAP_ACCESS_TOKEN
+var source = new EventSource('/topic/geodata_final123'); //ENTER YOUR TOPICNAME HERE
+source.addEventListener('message', function(e){
+
+  console.log('Message');
+  obj = JSON.parse(e.data);
+  console.log(obj);
+
+  if(obj.busline == '00001') {
+    for (var i = 0; i < mapMarkers1.length; i++) {
+      mymap.removeLayer(mapMarkers1[i]);
+    }
+    marker1 = L.marker([obj.latitude, obj.longitude]).addTo(mymap);
+    mapMarkers1.push(marker1);
+  }
+
+  if(obj.busline == '00002') {
+    for (var i = 0; i < mapMarkers2.length; i++) {
+      mymap.removeLayer(mapMarkers2[i]);
+    }
+    marker2 = L.marker([obj.latitude, obj.longitude]).addTo(mymap);
+    mapMarkers2.push(marker2);
+  }
+
+  if(obj.busline == '00003') {
+    for (var i = 0; i < mapMarkers3.length; i++) {
+      mymap.removeLayer(mapMarkers3[i]);
+    }
+    marker3 = L.marker([obj.latitude, obj.longitude]).addTo(mymap);
+    mapMarkers3.push(marker3);
+  }
+}, false);
